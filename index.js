909a8320f3f14b25138310eecf9dd39294e18c16
ashutosh <ashutosh.bharti5247@gmail.com>
2020-05-08 18:11:06 +0530

    fixed user validation

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express= require("express");
-
 const app= express();
+const mongoose = require("mongoose");
+const dotenv= require("dotenv");
 
+
+//routes
 const authRoute= require('./routes/auth');
 
+//congfig
+dotenv.config();
+
+//connecting to DB
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser : true},(DBerror)=>{
+    console.log("DB Error=>", DBerror)
+})
+
+
 //middlware
+app.use(express.json());
+
 app.use('/api/user', authRoute)
-app.listen(5000, ()=>{
+app.listen(3000, ()=>{
     console.log('sv running...')
 })
